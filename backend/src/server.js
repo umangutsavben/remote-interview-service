3f91fdbe46c15640b07850ce7daf0b55a77aa7f8
Umang Kumar <umangutsavben@gmail.com>
2026-09-01 04:35:07 +0530

    trying to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,29 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
-
-
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
-
-app.get("/",(req,res)=>{
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.get("/api", (req, res) => {
     res.status(200).json({
-        msg:"its done bro fjjjrom api"
+        msg: "its done bro fjjjrom api"
     })
 })
 
-app.listen(ENV.PORT,()=>{
+if (ENV.NODE_ENV === "kill -9 $(lsof -t -i :3000)") {
+    app.use(express.static(path.join(__dirname, "../../frontend/dist/index.html")));
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(
+            path.join(
+                __dirname,
+                "../../frontend/dist/index.html"
+            )
+        );
+    });
+}
+
+app.listen(ENV.PORT, () => {
     console.log("server is running o-n port 3000");
 })
